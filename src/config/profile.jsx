f496c9821f39bfd3cee178f75222ebf0853b5d06
adrayv <draym001@g.ucla.edu>
2018-03-12 21:57:55 -0700

    splitting off for the night

--- src/config/profile.jsx
+++ src/config/profile.jsx
@@ -170,5 +170,20 @@ export default {
 		'Sketch',
 		'Zeplin',
 		'Other',
+	],
+
+	interviewTimes: [
+		'10:00 AM',
+		'10:30 AM',
+		'11:00 AM',
+		'11:30 AM',
+		'12:00 PM',
+		'12:30 PM',
+		'01:00 PM',
+		'01:30 PM',
+		'02:00 PM',
+		'02:30 PM',
+		'03:00 PM',
+		'03:30 PM'
 	]
 }
